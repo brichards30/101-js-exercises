9f864875717fd72922784b05699fedfabc4c10f3
Glitch (101-exercises) <none>
2019-08-26 16:23:27 +0000

    🐫🙉 Checkpoint ./assert.js:282805/541 ./101-exercises.js:282805/22

--- assert.js
+++ assert.js
@@ -3,7 +3,10 @@ const assert = function(actual, expected, message = "") {
 
     // This compares primitive values and collections. If they differ, throw an error.
     if(JSON.stringify(actual) !== JSON.stringify(expected)) {
+        console.error('[assert] expected:    ' + expected)
+        console.error('[assert] instead got: ' + actual)
         throw Error("Assert failed. The expected and actual values are not the same. " + message);
+        
     }
     
 };
