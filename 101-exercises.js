800870e1524fb1e389122f49ee21f087e7480d0a
Glitch (101-exercises) <none>
2019-08-26 18:28:11 +0000

    Exporting to ryanorsinger/101-exercises-js ./101-exercises.js:282805/1215 ./index.html:282805/122

--- 101-exercises.js
+++ 101-exercises.js
@@ -1,5 +1,28 @@
 "use strict"; // leave this line here :)
 
+// Keep this function definition in order to test expected vs. actual results
+const assert = function(actual, expected, message = "") {
+
+    // This compares primitive values and collections. If they differ, throw an error.
+    if(JSON.stringify(actual) !== JSON.stringify(expected)) {
+        console.error('[assert] expected:    ' + expected)
+        console.error('[assert] instead got: ' + actual)
+        throw Error("Assert failed in " + message);
+    }
+};
+
+
+// Keep this function here to be able to add and count correct questions
+function addToDone(message) {
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(message);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    node.classList.add("finished");
+    document.querySelector(".correct ul").appendChild(node);
+    document.querySelector("#count").innerHTML = document.querySelectorAll('.finished').length + " of 101";
+}
+
+
 // Welcome to 101 Exercises in JS
 // If you get "ReferenceError: someVariableName is not defined", that means you need to create a variable
 
@@ -7,7 +30,7 @@
 // Example problem setup: Create a variable named doingJSRightNow and assign it the boolean true.
 // The line below creates the variable named doingJSRightNow and assigns the boolean value true
 // To complete Exercise #0, uncomment the following line of JS
-var doingJSRightNow = false
+// var doingJSRightNow = true
 
 // The lines below will test your answer. If you see an error, then it means that your answer is incorrect or incomplete.
 assert(doingJSRightNow, true, "Exercise 0"); 
